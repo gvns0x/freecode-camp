ffc151d314d1d54a391ebae8c075a6216c55a0ba
João Araújo <juauzaraujo@gmail.com>
2018-04-08 13:08:57 +0100

    input fields only accept numbers + return error msg

--- local weather/weather.js
+++ local weather/weather.js
@@ -6,36 +6,44 @@ let inputFields = Array.prototype.slice.call(document.getElementsByClassName("in
 let weatherArray = [];
 let labelArray = [];
 
-for(let j=0;j<inputLabel.length;j++) {
-  labelArray.push(inputLabel[j].innerHTML);
+for (let j = 0; j < inputLabels.length; j++) {
+  labelArray.push(inputLabels[j].innerHTML);
 }
 
-inputLabel
-
-function check(evt) {
-  var code = evt.charCode;
-  if (code != 0) {
-    if(code < 97 || code > 122) {
+inputFields.forEach(function(element, index) {
+  element.addEventListener("keypress", function check(evt) {
+    let pressedKey = evt.keyCode;
+    console.log(pressedKey);
+    if(pressedKey < 48 || pressedKey > 57) {
       evt.preventDefault();
-      inputLabels[0].innerHTML = "NOO";
-    }
-  }
-}
+        inputLabels[index].classList.add("input-card__label__error");
+        inputLabels[index].innerHTML = "Nope, only numbers";
+
+        setTimeout(function labelsBackToNormal() {
+          inputLabels[index].classList.remove("input-card__label__error");
+          inputLabels[index].innerHTML = labelArray[index];
+        }, 5000);
+      } else {
+        inputLabels[index].classList.remove("input-card__label__error");
+        inputLabels[index].innerHTML = labelArray[index];
+      }
+    })
+  });
 
 function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        weatherArray = JSON.parse(this.responseText);
-        getWeatherArray(weatherArray);
-      }
-    };
-    xhttp.open("GET", "https://fcc-weather-api.glitch.me/api/current?lon=55&lat=12", true);
-    xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      weatherArray = JSON.parse(this.responseText);
+      getWeatherArray(weatherArray);
+    }
+  };
+  xhttp.open("GET", "https://fcc-weather-api.glitch.me/api/current?lon=55&lat=12", true);
+  xhttp.send();
 }
 
 loadDoc();
 
 function getWeatherArray(weatherArray) {
-    console.log(weatherArray.coord);
+  console.log(weatherArray.coord);
 }
