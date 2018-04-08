e168cc0442a241f8e208a23c91c627a10f26555b
João Araújo <juauzaraujo@gmail.com>
2018-04-08 18:52:04 +0100

    values change according to requests to API

--- local weather/weather.js
+++ local weather/weather.js
@@ -42,8 +42,64 @@ function loadDoc() {
   xhttp.send();
 }
 
+/* Variables to show */
+
+    /* Location */
+let city = Array.prototype.slice.call(document.getElementsByClassName("weather-card__location__city"));
+let country = Array.prototype.slice.call(document.getElementsByClassName("weather-card__location__country"));
+
+    /* Temperature */
+let mainTemp = Array.prototype.slice.call(document.getElementsByClassName("weather-card__location__temp__maintemp"));
+let minTemp = Array.prototype.slice.call(document.getElementsByClassName("weather-card__location__temp__othertemp__minvalue"));
+let maxTemp = Array.prototype.slice.call(document.getElementsByClassName("weather-card__location__temp__othertemp__maxvalue"));
+
+
+    /* Weather */
+let mainWeather = Array.prototype.slice.call(document.getElementsByClassName("weather-card__weather__main"));
+let subWeather = Array.prototype.slice.call(document.getElementsByClassName("weather-card__weather__sub"));
+
+    /* Other data */
+let humidity = Array.prototype.slice.call(document.getElementsByClassName("weather-card__otherdata__humidityvalue"));
+let wind = Array.prototype.slice.call(document.getElementsByClassName("weather-card__otherdata__windvalue"));
+
+    /* Switch button */
+let switchBtn = Array.prototype.slice.call(document.getElementsByClassName("weather-card__location__temp__switch"));
+
 submitBtn[0].addEventListener("click", loadDoc);
 
+let tempFields = ["mainTemp[0].innerHTML", "minTemp[0].innerHTML", "maxTemp[0].innerHTML"];
+
 function getWeatherArray(weatherArray) {
+  city[0].innerHTML = weatherArray.name + ", ";
+  country[0].innerHTML = weatherArray.sys.country;
+
+  mainTemp[0].innerHTML = weatherArray.main.temp;
+  minTemp[0].innerHTML = weatherArray.main.temp_min;
+  maxTemp[0].innerHTML = weatherArray.main.temp_max;
+
+  mainWeather[0].innerHTML = weatherArray.weather[0].main;
+  subWeather[0].innerHTML = weatherArray.weather[0].description;
+
+  humidity[0].innerHTML = weatherArray.main.humidity;
+  wind[0].innerHTML = weatherArray.wind.speed;
+
   console.log(weatherArray.weather[0].id);
+
+  allTemps = [];
+  allTemps.push(weatherArray.main.temp, weatherArray.main.temp_min, weatherArray.main.temp_max);
+
+  function switchToFar() {
+    newTemps = allTemps.map(function(element) {
+      return element + 2000;
+    });
+    
+    for(let o = 0; o<allTemps.length;o++) {
+      tempFields[o] = newTemps[o];
+    }
+  }
+
+  switchToFar();
+
+
 }
+
