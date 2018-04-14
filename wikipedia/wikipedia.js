116cca49c6f8d990c2503e6ee43190ead67a69f4
João Araújo <juauzaraujo@gmail.com>
2018-04-14 23:59:51 +0100

    function logs new wikipedia API link on new input submition

--- wikipedia/wikipedia.js
+++ wikipedia/wikipedia.js
@@ -1,9 +1,8 @@
 let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0];
 let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search=";
 
-let searchUrl = wikiUrl + input.value;
-
-$(".searchbox__input").on("changed keyup", function() {
+$(".searchbox__input").change(function() {
+    let searchUrl = wikiUrl + input.value;
     console.log(searchUrl);
     console.log(input.value);
 })
