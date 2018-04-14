345028943d82cdf9dc06fbae8bfb4ccebe4b4a17
João Araújo <juauzaraujo@gmail.com>
2018-04-14 21:04:46 +0100

    functions added - not working 100%

--- wikipedia/wikipedia.js
+++ wikipedia/wikipedia.js
@@ -1,4 +1,10 @@
-let inputValue = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0].value;
-let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search="
+let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0];
+let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search=";
 
+let searchUrl = wikiUrl + input.value;
+
+$(".searchbox__input").on("changed keyup", function() {
+    console.log(searchUrl);
+    console.log(input.value);
+})
 
