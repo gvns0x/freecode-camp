26ff0a505dc51a26cadb27860481deb4ac5919e3
João Araújo <juauzaraujo@gmail.com>
2018-04-14 19:51:33 +0100

    inputValue variable changed

--- wikipedia/wikipedia.js
+++ wikipedia/wikipedia.js
@@ -1,2 +1,4 @@
-let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"));
+let inputValue = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0].value;
+let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search="
+
 
