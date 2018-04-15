3dbcc92240e898cec01d4c0d135267267f496934
João Araújo <juauzaraujo@gmail.com>
2018-04-15 09:30:50 +0100

    added button for search

--- wikipedia/wikipedia.js
+++ wikipedia/wikipedia.js
@@ -1,9 +1,13 @@
 let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0];
 let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search=";
+let button = Array.prototype.slice.call(document.getElementsByClassName("searchbox__button"))[0];
 
-$(".searchbox__input").change(function() {
+/* When input changes, get new searchUrl */
+$(".searchbox__input").change(generateUrl);
+button.addEventListener("click", generateUrl);
+
+function generateUrl() {
     let searchUrl = wikiUrl + input.value;
     console.log(searchUrl);
     console.log(input.value);
-})
-
+}
