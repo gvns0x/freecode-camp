ad4769169b72fc352df787098e6c20e3a7c94d97
João Araújo <juauzaraujo@gmail.com>
2018-04-15 12:09:50 +0100

    new buttons + new html structure

--- wikipedia/wikipedia.js
+++ wikipedia/wikipedia.js
@@ -1,7 +1,7 @@
 function setup() {
-    let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0];
+    let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__search__main__input"))[0];
     let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search=";
-    let button = Array.prototype.slice.call(document.getElementsByClassName("searchbox__button"))[0];
+    let button = Array.prototype.slice.call(document.getElementsByClassName("searchbox__search__main__button"))[0];
 
     /* When input changes, get new searchUrl */
     button.addEventListener("click", generateUrl);
