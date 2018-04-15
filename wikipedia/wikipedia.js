e250447f82b8d72af02a7765d1cc6f93278fd3f4
João Araújo <juauzaraujo@gmail.com>
2018-04-15 09:48:36 +0100

    getting data from wikipedia api

--- wikipedia/wikipedia.js
+++ wikipedia/wikipedia.js
@@ -1,13 +1,20 @@
-let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0];
-let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search=";
-let button = Array.prototype.slice.call(document.getElementsByClassName("searchbox__button"))[0];
+function setup() {
+    let input = Array.prototype.slice.call(document.getElementsByClassName("searchbox__input"))[0];
+    let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&search=";
+    let button = Array.prototype.slice.call(document.getElementsByClassName("searchbox__button"))[0];
 
-/* When input changes, get new searchUrl */
-$(".searchbox__input").change(generateUrl);
-button.addEventListener("click", generateUrl);
+    /* When input changes, get new searchUrl */
+    button.addEventListener("click", generateUrl);
 
-function generateUrl() {
-    let searchUrl = wikiUrl + input.value;
-    console.log(searchUrl);
-    console.log(input.value);
+    function generateUrl() {
+        let searchUrl = wikiUrl + input.value;
+        console.log(searchUrl);
+        console.log(input.value);
+
+        loadJSON(searchUrl, getData, 'jsonp');
+
+        function getData(data) {
+            console.log(data);
+        }
+    }
 }
