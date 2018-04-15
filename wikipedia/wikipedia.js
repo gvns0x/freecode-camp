871d709e56b0108982342d8143ff0bbd6e85fde6
João Araújo <juauzaraujo@gmail.com>
2018-04-15 11:55:45 +0100

    add ... if description is too big

--- wikipedia/wikipedia.js
+++ wikipedia/wikipedia.js
@@ -19,12 +19,26 @@ function setup() {
             let searchLinks = data["3"];
             console.log(data);
 
+
             $(".searchbox__results").append("<h1 class='searchbox__results__intro'>There are " + (searchLinks.length - 1) + " results for " + searchTitles[0] + "</h1>");
 
             for (let i = 0; i < searchTitles.length - 1; i++) {
+                let descLength = 150;
                 $(".searchbox__results").append("<a href='" + searchLinks[i + 1] + "' target='_blank'> <div class='searchbox__results__item'> <h2 class='searchbox__results__title'>"
                 + searchTitles[i+1] +
-                "</h2><p class='searchbox__results__description'>" + searchDescriptions[i + 1] + "</div></a>");
+                "</h2><p class='searchbox__results__description'>" + smallDesc() + "</div></a>");
+
+                /* If the description is bigger or == to descLength, add "..." at the end */
+                function smallDesc() {
+                    let brief = searchDescriptions[i + 1].slice(0,descLength);
+                    if(searchDescriptions[i+1].length < descLength) {
+                        return searchDescriptions[i + 1];
+                    } else {
+                        return brief + "...";
+                    }
+                    console.log(brief.length);
+                }
+                smallDesc();
             }
         }
     }
